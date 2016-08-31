53328c3f4e4837fd996918c76a23e178fa97e5e2
Timur Uzel <timurt.uzel@gmail.com>
2016-08-31 13:44:38 -0400

    removed domali

--- psydux.js
+++ psydux.js
@@ -2,18 +2,17 @@
  * Created by timur on 8/31/16.
  */
 
-const dom = require('domali')
-
 const psy = {}
-
 
 psy.el = function(tag, attributes, callback) {
   
-  const element = dom.create(tag)
+  const e = document.createElement(tag)
   
-  element.set(attributes)
+  for(const a in attributes) {
+    e.setAttribute(a, attributes[a])
+  }
   
-  dom.render(element)
+  document.body.appendChild(element)
   
   return callback(element)
 }
