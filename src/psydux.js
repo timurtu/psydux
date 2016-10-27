ac545be307ebef4dbbb0d558253fe922b00ae4b9
Timur Uzel <timur@Timurs-MacBook-Pro.local>
2016-10-27 02:14:29 -0400

    check for callback

--- src/psydux.js
+++ src/psydux.js
@@ -5,40 +5,44 @@
 const psy = {}
 
 psy.el = function (tag, attributes, callback) {
-  
+
   const e = document.createElement(tag)
-  const returnValue = callback()
-  
+
   for (const a in attributes) {
     e.setAttribute(a, attributes[a])
   }
-  
-  /**
-   * Switch on the typeof returnValue to narrow down type
-   */
-  switch (typeof returnValue) {
-    
-    /* they returned a list of elements */
-    case 'object':
-      switch (tag) {
-        default:
-          Array.prototype.forEach.call(returnValue, element => e.appendChild(element))
-      }
-      break
-    
-    /* they returned a string */
-    case 'string':
-    
-    default:
-      /* Switch on the tag to decide how to change the node's text */
-      switch (tag) {
-        default:
-          e.appendChild(document.createTextNode(returnValue))
-      }
+
+  if(callback) {
+
+    const returnValue = callback()
+
+    /**
+     * Switch on the returnValue to narrow down type
+     */
+    switch (typeof returnValue) {
+
+      /* they returned a list of elements */
+      case 'object':
+        switch (tag) {
+          default:
+            Array.prototype.forEach.call(returnValue, element => e.appendChild(element))
+        }
+        break
+
+      /* they returned a string */
+      case 'string':
+
+      default:
+        /* Switch on the tag to decide how to change the node's text */
+        switch (tag) {
+          default:
+            e.appendChild(document.createTextNode(returnValue))
+        }
+    }
   }
-  
+
   document.body.appendChild(e)
   return e
 }
 
-export default psy
+module.exports = psy
