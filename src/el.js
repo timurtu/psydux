191730004e8ab97c8572b3355f4b3578f7f93eae
Timur Uzel <timur@Timurs-MacBook-Pro.local>
2016-10-27 05:30:22 -0400

    redundant comments

--- src/el.js
+++ src/el.js
@@ -13,21 +13,14 @@ const el = function (tag, attributes, callback) {
     const returnValue = callback()
 
     switch (typeof returnValue) {
-
-      /* they returned an element a list of elements */
       case 'object':
-
         switch (tag) {
           default:
             Array.prototype.forEach.call(returnValue, element => e.appendChild(element))
         }
         break
-
-      /* they returned a string */
       case 'string':
-
       default:
-        /* Switch on the tag to decide how to change the node's text */
         switch (tag) {
           default:
             e.appendChild(document.createTextNode(returnValue))
