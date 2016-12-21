6db79b07421968c2a1bda32f19e30f6623bc9be5
Timur Uzel <timur@Timurs-MacBook-Pro.local>
2016-12-21 17:12:43 -0500

    state is hard

--- dist/state.js
+++ dist/state.js
@@ -1,17 +1,8 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-
-var _domali = require('domali');
-
-var _domali2 = _interopRequireDefault(_domali);
-
-var _el = require('./el');
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
 /**
  * Created by timur on 12/19/2016.
  */
@@ -26,16 +17,6 @@ function get() {
 function set(nextState) {
   states.push(nextState);
   count++;
-  if (_el.components) {
-    update(_el.components);
-  }
-}
-
-function update(components) {
-
-  components.forEach(function (comp) {
-    return _domali2.default.render(comp.node);
-  });
 }
 
 exports.default = {
--- src/state.js
+++ src/state.js
@@ -2,9 +2,6 @@
  * Created by timur on 12/19/2016.
  */
 
-import dom from 'domali'
-import { components } from './el'
-
 const states = [{}]
 let count = 0
 
@@ -15,15 +12,6 @@ function get() {
 function set(nextState) {
   states.push(nextState)
   count++
-  if(components) {
-    update(components)
-  }
-}
-
-function update(components) {
-
-  components.forEach(comp => dom.render(comp.node))
-
 }
 
 export default {
