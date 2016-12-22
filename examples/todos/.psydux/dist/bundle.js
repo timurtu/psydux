54187c4844c8ccac826fb9ba81a6753c2087dace
Timur Uzel <timur@Timurs-MacBook-Pro.local>
2016-12-21 21:12:43 -0500

    unstateful todo list

--- examples/todos/.psydux/dist/bundle.js
+++ examples/todos/.psydux/dist/bundle.js
@@ -57,11 +57,17 @@
 	    return elements;
 	  }, { class: 'container-fluid' });
 	};
+
 	var title = function title(text) {
 	  return (0, _psydux.el)('h1', function () {
 	    return text;
 	  });
 	};
+
+	var input = function input(placeholder) {
+	  return (0, _psydux.el)('input', { placeholder: placeholder, class: 'form-control' });
+	};
+
 	var form = function form() {
 	  for (var _len2 = arguments.length, elements = Array(_len2), _key2 = 0; _key2 < _len2; _key2++) {
 	    elements[_key2] = arguments[_key2];
@@ -71,16 +77,7 @@
 	    return elements;
 	  });
 	};
-	var input = function input(placeholder) {
-	  return (0, _psydux.el)('input', { placeholder: placeholder, class: 'form-control' });
-	};
-	var button = function button(_ref) {
-	  var type = _ref.type,
-	      text = _ref.text;
-	  return (0, _psydux.el)('button', function () {
-	    return text;
-	  }, { class: 'btn btn-' + type + ' btn-block' });
-	};
+
 	var row = function row() {
 	  for (var _len3 = arguments.length, elements = Array(_len3), _key3 = 0; _key3 < _len3; _key3++) {
 	    elements[_key3] = arguments[_key3];
@@ -90,6 +87,7 @@
 	    return elements;
 	  }, { class: 'row' });
 	};
+
 	var col = function col(amount, display) {
 	  for (var _len4 = arguments.length, elements = Array(_len4 > 2 ? _len4 - 2 : 0), _key4 = 2; _key4 < _len4; _key4++) {
 	    elements[_key4 - 2] = arguments[_key4];
@@ -100,19 +98,51 @@
 	  }, { class: 'col-' + display + '-' + amount });
 	};
 
+	var button = function button(_ref) {
+	  var type = _ref.type,
+	      text = _ref.text;
+	  return (0, _psydux.el)('button', function () {
+	    return text;
+	  }, { class: 'btn btn-' + type + ' btn-block' });
+	};
+
 	var todoInput = input('Add Todo');
 
 	var todoForm = form(row(col(8, 'xs', todoInput), col(4, 'xs', button({ type: 'success', text: 'Add' }))));
 
-	var todoList = (0, _psydux.el)('ul');
+	var todoList = (0, _psydux.el)('div', { class: 'list-group' });
+
+	var todoItem = function todoItem(item) {
+	  return (0, _psydux.el)('a', function () {
+	    return item;
+	  }, {
+	    class: 'list-group-item',
+	    role: 'button'
+	  });
+	};
 
 	todoForm.onsubmit = function (e) {
+
 	  e.preventDefault();
-	  todoList.push(todoInput.value);
+
+	  if (todoInput.value === '') {
+	    return;
+	  }
+
+	  var todo = todoItem(todoInput.value);
+
+	  todo.onclick = function () {
+	    this.style.textDecoration = this.style.textDecoration === 'none' ? 'line-through' : 'none';
+	  };
+
+	  todoList.unshift(todo);
+
 	  todoInput.value = '';
 	};
 
-	(0, _psydux.render)(container(title('Todo List'), todoForm, todoList));
+	(0, _psydux.render)(container(title('Todo List'), todoForm, (0, _psydux.el)('hr'), todoList));
+
+	todoInput.focus();
 
 /***/ },
 /* 1 */
@@ -127,27 +157,27 @@
 
 	__webpack_require__(2);
 
-	var _el = __webpack_require__(299);
+	__webpack_require__(299);
+
+	var _el = __webpack_require__(306);
 
 	var _el2 = _interopRequireDefault(_el);
 
-	var _state = __webpack_require__(300);
+	var _state = __webpack_require__(307);
 
 	var _state2 = _interopRequireDefault(_state);
 
-	var _render = __webpack_require__(301);
+	var _render = __webpack_require__(308);
 
 	var _render2 = _interopRequireDefault(_render);
 
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-	/**
-	 * Created by timur on 8/31/16.
-	 */
-
 	exports.el = _el2.default;
 	exports.state = _state2.default;
-	exports.render = _render2.default;
+	exports.render = _render2.default; /**
+	                                    * Created by timur on 8/31/16.
+	                                    */
 
 /***/ },
 /* 2 */
@@ -8291,6 +8321,244 @@
 
 /***/ },
 /* 299 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+
+	var _get = __webpack_require__(300);
+
+	var _select = __webpack_require__(301);
+
+	var _create = __webpack_require__(302);
+
+	var _render = __webpack_require__(303);
+
+	var _node = __webpack_require__(304);
+
+	__webpack_require__(305);
+
+	/**
+	 * Created by timur on 4/27/16.
+	 */
+
+	exports.default = {
+
+	  getId: _get.getId,
+	  create: _create.create,
+	  getClass: _get.getClass,
+	  getTags: _get.getTags,
+	  select: _select.select,
+	  selectAll: _select.selectAll,
+	  render: _render.render,
+	  clear: _render.clear,
+	  clone: _node.clone
+	};
+
+/***/ },
+/* 300 */
+/***/ function(module, exports) {
+
+	"use strict";
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	/**
+	 * Created by timur on 5/22/16.
+	 */
+
+	/**
+	 * Return one or many elements by getting them by id.
+	 *
+	 * @param ids Any number of id strings
+	 * @returns elements as an array
+	 */
+	var getId = exports.getId = function getId() {
+	  for (var _len = arguments.length, ids = Array(_len), _key = 0; _key < _len; _key++) {
+	    ids[_key] = arguments[_key];
+	  }
+
+	  return ids.length > 1 ? ids.map(function (id) {
+	    return document.getElementById(id);
+	  }) : document.getElementById(ids[0]);
+	};
+
+	var getClass = exports.getClass = function getClass(className) {
+	  return document.getElementsByClassName(className);
+	};
+	var getTags = exports.getTags = function getTags(tagName) {
+	  return document.getElementsByTagName(tagName);
+	};
+
+/***/ },
+/* 301 */
+/***/ function(module, exports) {
+
+	"use strict";
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	/**
+	 * Created by timur on 7/24/16.
+	 */
+
+	var select = exports.select = function select(query) {
+	  return document.querySelector(query);
+	};
+	var selectAll = exports.selectAll = function selectAll(query) {
+	  return document.querySelectorAll(query);
+	};
+
+/***/ },
+/* 302 */
+/***/ function(module, exports) {
+
+	"use strict";
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	/**
+	 * Created by timur on 7/24/16.
+	 */
+
+	/**
+	 * Return one or many elements created with their tagname
+	 *
+	 * @param elements Any number of element tag strings
+	 * @returns elements as an array
+	 */
+	var create = exports.create = function create() {
+	  for (var _len = arguments.length, elements = Array(_len), _key = 0; _key < _len; _key++) {
+	    elements[_key] = arguments[_key];
+	  }
+
+	  var domNodes = elements.map(function (el) {
+	    return document.createElement(el);
+	  });
+	  return domNodes.length > 1 ? domNodes : domNodes[0];
+	};
+
+/***/ },
+/* 303 */
+/***/ function(module, exports) {
+
+	"use strict";
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	/**
+	 * Created by timur on 5/24/16.
+	 */
+
+	var render = exports.render = function render(element) {
+	  return document.body.appendChild(element);
+	};
+	var clear = exports.clear = function clear() {
+	  return document.removeChild(document.documentElement);
+	};
+
+/***/ },
+/* 304 */
+/***/ function(module, exports) {
+
+	"use strict";
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	/**
+	 * Created by timur on 5/24/16.
+	 */
+
+	var clone = exports.clone = function clone(node) {
+	  return node.cloneNode(true);
+	};
+
+	Node.prototype.set = function (props) {
+	  for (var key in props) {
+	    this.setAttribute(key, props[key]);
+	  }
+	  return this;
+	};
+
+	Node.prototype.get = function () {
+	  var _this = this;
+
+	  for (var _len = arguments.length, keys = Array(_len), _key = 0; _key < _len; _key++) {
+	    keys[_key] = arguments[_key];
+	  }
+
+	  if (keys.length === 1) {
+	    return this.getAttribute(keys[0]);
+	  }
+
+	  return keys.map(function (key) {
+	    return _this.getAttribute(key);
+	  });
+	};
+
+	Node.prototype.text = function (msg) {
+
+	  this.textContent = msg;
+	  return this;
+	};
+
+/***/ },
+/* 305 */
+/***/ function(module, exports) {
+
+	"use strict";
+
+	var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol ? "symbol" : typeof obj; };
+
+	/**
+	 * Created by timur on 8/17/16.
+	 */
+
+	Node.prototype.push = function (child) {
+
+	  this.appendChild(child);
+
+	  return this;
+	};
+
+	Node.prototype.unshift = function (child) {
+
+	  this.insertBefore(child, this.firstChild);
+
+	  return this;
+	};
+
+	Node.prototype.map = function (callback) {
+	  var _this = this;
+
+	  if (this.hasChildNodes()) {
+	    var _ret = function () {
+
+	      var newNode = _this.cloneNode(true);
+
+	      Array.prototype.map.call(newNode.childNodes, function (node) {
+	        return newNode.replaceChild(callback(node), node);
+	      });
+
+	      return {
+	        v: newNode
+	      };
+	    }();
+
+	    if ((typeof _ret === "undefined" ? "undefined" : _typeof(_ret)) === "object") return _ret.v;
+	  }
+	};
+
+/***/ },
+/* 306 */
 /***/ function(module, exports) {
 
 	'use strict';
@@ -8349,7 +8617,7 @@
 	};
 
 /***/ },
-/* 300 */
+/* 307 */
 /***/ function(module, exports) {
 
 	"use strict";
@@ -8378,7 +8646,7 @@
 	};
 
 /***/ },
-/* 301 */
+/* 308 */
 /***/ function(module, exports) {
 
 	'use strict';
--- examples/todos/src/app.js
+++ examples/todos/src/app.js
@@ -1,12 +1,18 @@
-import { el, state, render } from 'psydux'
+import { el, render } from 'psydux'
 
 const container = (...elements) => el('div', () => elements, { class: 'container-fluid' })
+
 const title = text => el('h1', () => text)
-const form = (...elements) => el('form', () => elements)
+
 const input = placeholder => el('input', { placeholder, class: 'form-control' })
-const button = ({ type, text }) => el('button', () => text, { class: `btn btn-${type} btn-block` })
+
+const form = (...elements) => el('form', () => elements)
+
 const row = (...elements) => el('div', () => elements, { class: 'row' })
+
 const col = (amount, display, ...elements) => el('div', () => elements, { class: `col-${display}-${amount}` })
+
+const button = ({ type, text }) => el('button', () => text, { class: `btn btn-${type} btn-block` })
 
 const todoInput = input('Add Todo')
 
@@ -17,11 +23,29 @@ const todoForm = form(
   )
 )
 
-const todoList = el('ul')
+const todoList = el('div', { class: 'list-group' })
+
+const todoItem = item => el('a', () => item, {
+  class: 'list-group-item',
+  role: 'button',
+})
 
 todoForm.onsubmit = e => {
+
   e.preventDefault()
-  todoList.push(todoInput.value)
+
+  if(todoInput.value === '') {
+    return
+  }
+
+  const todo = todoItem(todoInput.value)
+
+  todo.onclick = function() {
+    this.style.textDecoration = this.style.textDecoration === 'none' ? 'line-through' : 'none'
+  }
+
+  todoList.unshift(todo)
+
   todoInput.value = ''
 }
 
@@ -29,7 +53,9 @@ render(
   container(
     title('Todo List'),
     todoForm,
+    el('hr'),
     todoList
   )
 )
 
+todoInput.focus()
